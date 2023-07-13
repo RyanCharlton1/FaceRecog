84a7a687502753b3f0ff24c0bd240cc5266c8ba8
Ryan Charlton <ryancharlton271@gmail.com>
2023-07-13 21:35:49 +0100

    fix: eigenfaces: fixed mean face

--- Eigenfaces/geneigenface.m
+++ Eigenfaces/geneigenface.m
@@ -13,19 +13,27 @@ function out=geneigenface(setpath)
 
     % convert array to doubles for upcoming
     img = double(img);
+    % find mean
+    mean = zeros(1, 64 * 64);
+    for i=3:n
+        mean = mean + (1/(n-2))*img(i-2,:);
+    end
     % make mean subtracted images
-    img = img - mean(img);
-
+    for i=1:n-2
+        img(i, :) = img(i, :) - mean;
+    end
+    %img = img - mean(img);
+    imshow(mat2gray(reshape(mean, [64, 64])))
     % calc covariance 
-    c = img * img';
+    c = img' * img;
     %c = cov(img);
     % calc eigen vectors and values
     [evec, eval] = eig(c);
-    
-    faces = zeros(n - 2, 64 * 64, 'int8');
-    for i = 1:(n-2)
-        faces(i-2) = evec(:, end-(i-3))' * img;
-    end
 
-    out=faces;
+    %faces = zeros(n - 2, 64 * 64, 'double');
+    %for i = 1:(n-2)
+    %    faces(i, :) = evec(:, end-(i-1))' * img;
+    %end
+
+    out=evec;
 end
